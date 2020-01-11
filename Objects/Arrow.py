d8656862f5a48e6751558318ee62ed5afde57643
Kosko12 <48369321+Kosko12@users.noreply.github.com>
2020-01-11 15:00:38 +0100

    Add files via upload

---
+++ Objects/Arrow.py
@@ -0,0 +1,15 @@
+
+
+class Arrow():
+
+    def __init__(self, x, y, xRange, yRange, image):
+        # pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.xRange = xRange
+        self.yRange = yRange
+        self.x = x
+        self.y = y
+
+    def update(self, *args):
+        self.x = args[0]
+        self.y = args[1]
---
+++ Objects/Player.py
@@ -0,0 +1,25 @@
+import pygame
+
+
+class Player():
+
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.image = ""
+
+    def __init__(self, x, y, xRange, yRange, image):
+        #pygame.sprite.Sprite.__init__(self)
+        self.image = image
+        self.xRange = xRange
+        self.yRange = yRange
+        self.x = x
+        self.y = y
+
+    def update(self, *args):
+        self.x = args[0]
+        self.y = args[1]
+
+    def print_self(self):
+        print("X: " + str(self.x) + " Y: " + str(self.y))
