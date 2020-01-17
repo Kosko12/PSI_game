02b4dcbb58b0789d136e17076123917708956fbb
Piotr <piotrkrz12@gmail.com>
2020-01-17 11:32:27 +0100

    Proba detekcji strzal(niestablina)

--- Objects/Player.py
+++ Objects/Player.py
@@ -4,18 +4,15 @@ import pygame
 class Player():
 
 
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.image = ""
 
-    def __init__(self, x, y, xRange, yRange, image, speed):
+    def __init__(self, x, y, xRange, yRange, image, maxSpeed):
         self.image = image
         self.xRange = xRange
         self.yRange = yRange
         self.x = x
         self.y = y
-        self.speed = speed
+        self.speed = 0
+        self.maxSpeed = maxSpeed
 
     def update(self, *args):
         self.x = args[0]
--- game.py
+++ game.py
@@ -7,6 +7,7 @@ import pygame
 import Arrow
 import Player
 import StopableThread
+import Map
 
 # initializers
 
@@ -32,8 +33,78 @@ arrowExist = True
 playerXchange = 0
 arrowYchange = 3
 
+lock = threading.Lock()
+
+arrow_for_player = []
+def if_in_danger(p):
+    while collapse == False:
+        global arrows
+        left = 0
+        right = 800
+        lock.acquire()
+        if len(arrows) > 5:
+            for ar in arrows:
+                if p.x - 1 <= ar.x and (p.x + p.xRange) + 1 > ar.x:
+                    for a1 in arrows:
+                        clRight, clLeft = get_closest(a1, arrows)
+                        if if_match(clLeft, a1.x, p.x + p.xRangex):
+                            if p.x > clLeft:
+                                p.speed = -1 * p.maxSpeed
+
+                        elif if_match(a1.x, clRight, p.x + p.xRangex):
+                            if p.x < clRight:
+                                p.speed = 1 * p.maxSpeed
+
+                else:
+                    p.speed = 0
+            lock.release()
+        else:
+            for ar in arrows:
+                if p.x - 1 <= ar.x and (p.x + p.xRange) + 1 > ar.x:
+                    for closest in arrows:
+                        if closest != ar:
+                            if closest.x < ar.x:
+                                if closest.x > left:
+                                    left = closest.x
+                            else:
+                                if closest.x < right:
+                                    right = closest.x
+                        else:
+                            if p.speed == 0:
+                                if 800 - p.x < p.x + p.xRange:
+                                    p.speed = -1. * p.maxSpeed
+                                else:
+                                    p.speed = p.maxSpeed
+                    if if_match(ar.x, right, p.x + p.xRange):
+                        if p.speed <= 0:
+                            p.speed = p.maxSpeed
+                    elif if_match(left, ar.x, p.x + p.xRange):
+                        if p.speed >= 0:
+                            p.speed = -1. * p.maxSpeed
+                else:
+                    p.speed = 0
+            lock.release()
 # fun showing element
 
+def get_closest(a, arrow):
+    left = 0
+    right = 800
+    if len(arrows) > 2:
+        for ar in arrows:
+            for closest in arrows:
+                if closest != ar:
+                    if closest.x < ar.x:
+                        if closest.x > left:
+                            left = closest
+                    else:
+                        if closest.x < right:
+                            right = closest
+        return left, right
+def if_match(x_o, x_k, r):
+    sub = x_k - x_o
+    if sub > r:
+        return True
+    return False
 
 def show_player(p, x, y):
 
@@ -57,7 +128,7 @@ def if_in_range(p, a):
 
 
 def load_player(x_pos):
-    pl = Player.Player(x_pos, playerY, 84., 31., 'ludek.png', random.uniform(-0.2, 0.2))
+    pl = Player.Player(x_pos, playerY, 84., 31., 'ludek.png', random.uniform(1.5, 2.))
     return pl
 
 
@@ -70,13 +141,15 @@ collapse = False
 def load_arrow(timeGap):
     checker = True
     while checker:
-
+        lock.acquire()
         xPos = random.randrange(800)
-        arrows.append(Arrow.Arrow(xPos, 0., 18., 28., 'arrow.png'))
+        ar = Arrow.Arrow(xPos, 0., 18., 28., 'arrow.png')
+        arrows.append(ar)
         if timeGap >= .55:
             timeGap -= .25
-        print(timeGap)
-        print(len(arrows))
+        lock.release()
+        #print(timeGap)
+        #print(len(arrows))
         time.sleep(timeGap)
         if collapse:
             checker = False
@@ -84,10 +157,10 @@ def load_arrow(timeGap):
 # loading players in amount of declared population
 
 
-population = 10
-generations = 5
+population = 2
+generations = 4
 
-
+player = []
 thread = StopableThread.StopableThread(target=load_arrow, args=(4,), daemon=True)
 
 
@@ -96,7 +169,7 @@ def main(thr):
     ifExit = False
     global collapse
     collapse = False
-    player = []
+    global player
     for x in range(population):
         player.append(load_player(random.uniform(0., 800.)))
     thr.start()
@@ -120,6 +193,7 @@ def main(thr):
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     playerXchange = 0
         for xp in player:
+            threading.Thread(target=if_in_danger, args=(xp,), daemon=True).start()
             xp.x += xp.speed
             show_player(xp, xp.x, xp.y)
             if xp.x <= 0:
@@ -134,12 +208,12 @@ def main(thr):
                 show_player(arr, arr.x, arr.y)
             if if_in_range(player, arr) and arr.y <= 600:
                 if generations > 0:
-
-
                     collapse = True
                     thr.join()
                     for ar in arrows:
                         arrows.remove(ar)
+                    for pl in player:
+                        player.remove(pl)
                     generations = generations - 1
                     main(StopableThread.StopableThread(target=load_arrow, args=(4,), daemon=True))
 
@@ -150,7 +224,7 @@ def main(thr):
                     sys.exit()
             elif arr.y > 600:
                 arrows.remove(arr)
-                print("Arrow removed!")
+                #print("Arrow removed!")
 
         #if arrowY < 600:
         #    showPlayer(arrow, arrowX, arrowY)
