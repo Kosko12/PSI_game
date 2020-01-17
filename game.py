7e74a8c6b1e2d4500ae3d0f2bc7cf54e2d5e44a6
Piotr <piotrkrz12@gmail.com>
2020-01-17 17:30:36 +0100

    W miare stabilny algorytm z naprawionymi wątkami

--- game.py
+++ game.py
@@ -34,71 +34,62 @@ playerXchange = 0
 arrowYchange = 3
 
 lock = threading.Lock()
-
+busy = {}
 arrow_for_player = []
 def if_in_danger(p):
     while collapse == False:
-        global arrows
+        lock.acquire()
+        loc_a = arrows
+        lock.release()
         left = 0
         right = 800
-        lock.acquire()
-        if len(arrows) > 5:
-            for ar in arrows:
-                if p.x - 1 <= ar.x and (p.x + p.xRange) + 1 > ar.x:
-                    for a1 in arrows:
-                        clRight, clLeft = get_closest(a1, arrows)
-                        if if_match(clLeft, a1.x, p.x + p.xRangex):
-                            if p.x > clLeft:
-                                p.speed = -1 * p.maxSpeed
+        guard = False
 
-                        elif if_match(a1.x, clRight, p.x + p.xRangex):
-                            if p.x < clRight:
-                                p.speed = 1 * p.maxSpeed
-
-                else:
-                    p.speed = 0
-            lock.release()
-        else:
-            for ar in arrows:
-                if p.x - 1 <= ar.x and (p.x + p.xRange) + 1 > ar.x:
-                    for closest in arrows:
-                        if closest != ar:
-                            if closest.x < ar.x:
-                                if closest.x > left:
-                                    left = closest.x
-                            else:
-                                if closest.x < right:
-                                    right = closest.x
+        for ar in loc_a:
+            if p.x - 1 <= ar.x and (p.x + p.xRange) + 1 > ar.x:
+                guard = 1
+                busy.update({p: 1})
+                for closest in loc_a:
+                    if closest != ar:
+                        if closest.x < ar.x:
+                            if closest.x > left:
+                                left = closest.x
                         else:
-                            if p.speed == 0:
-                                if 800 - p.x < p.x + p.xRange:
-                                    p.speed = -1. * p.maxSpeed
-                                else:
-                                    p.speed = p.maxSpeed
-                    if if_match(ar.x, right, p.x + p.xRange):
-                        if p.speed <= 0:
-                            p.speed = p.maxSpeed
-                    elif if_match(left, ar.x, p.x + p.xRange):
-                        if p.speed >= 0:
-                            p.speed = -1. * p.maxSpeed
-                else:
+                            if closest.x < right:
+                                right = closest.x
+                    else:
+                        if p.speed == 0:
+                            if 800 - p.x < p.x + p.xRange:
+                                p.speed = -1. * p.maxSpeed
+                            else:
+                                p.speed = p.maxSpeed
+                if if_match(ar.x, right, p.x + p.xRange):
+                    if p.speed <= 0:
+                        p.speed = p.maxSpeed
+                elif if_match(left, ar.x, p.x + p.xRange):
+                    if p.speed >= 0:
+                        p.speed = -1. * p.maxSpeed
+            else:
+                if not guard:
+                    busy.update({p: 0})
+                if busy.get(p) == 0:
                     p.speed = 0
-            lock.release()
+
 # fun showing element
 
 def get_closest(a, arrow):
     left = 0
     right = 800
-    if len(arrows) > 2:
-        for ar in arrows:
-            for closest in arrows:
+    if len(arrow) > 2:
+        for ar in arrow:
+            for closest in arrow:
                 if closest != ar:
                     if closest.x < ar.x:
                         if closest.x > left:
-                            left = closest
+                            left = closest.x
                     else:
                         if closest.x < right:
-                            right = closest
+                            right = closest.x
         return left, right
 def if_match(x_o, x_k, r):
     sub = x_k - x_o
@@ -121,8 +112,8 @@ def if_in_range(p, a):
     for e in p:
         for i in range(int(e.x), int(e.x + e.xRange)):
             if int(a.x) == i and (arrow.y == e.y - e.yRange or a.y > e.y - e.yRange):
-                return True
-    return False
+                p.remove(e)
+
 
 # fun which loads arrows/players
 
@@ -138,44 +129,80 @@ arrow = Arrow.Arrow(arrowX, arrowY, 18., 28., 'arrow.png')
 
 collapse = False
 
+
 def load_arrow(timeGap):
     checker = True
+
     while checker:
         lock.acquire()
         xPos = random.randrange(800)
-        ar = Arrow.Arrow(xPos, 0., 18., 28., 'arrow.png')
-        arrows.append(ar)
+        if len(arrows) < 15:
+            ar = Arrow.Arrow(xPos, 0., 18., 28., 'arrow.png')
+            arrows.append(ar)
+
         if timeGap >= .55:
             timeGap -= .25
+
         lock.release()
-        #print(timeGap)
-        #print(len(arrows))
-        time.sleep(timeGap)
+        time.sleep(0.45)
+
         if collapse:
             checker = False
 # main function
 # loading players in amount of declared population
 
 
-population = 2
+population = 8
 generations = 4
+gen_counter = 1
 
 player = []
 thread = StopableThread.StopableThread(target=load_arrow, args=(4,), daemon=True)
 
-
+player_threads = []
 # program loop
+
+
 def main(thr):
+    lock.acquire()
+    loc_arr = arrows
+    lock.release()
     ifExit = False
     global collapse
+    global busy
+    global gen_counter
     collapse = False
     global player
+    busy.clear()
+    if len(player) > 0:
+        for p in player:
+            player.remove(p)
     for x in range(population):
         player.append(load_player(random.uniform(0., 800.)))
+        tmp = {x: 0}
+        busy.update(tmp)
+    xa = busy
+    if len(loc_arr) > 0:
+        for a in loc_arr:
+            loc_arr.remove(a)
+
+    print(xa)
+
+    for p in player:
+        t = threading.Thread(target=if_in_danger, args=(p,), daemon=True)
+        player_threads.append(t)
+        t.start()
     thr.start()
+
     while not ifExit:
         # screen.fill((255, 255, 255))
         screen.blit(pygame.image.load('bg.png'), [0, 0])
+        font = pygame.font.Font('freesansbold.ttf',14)
+        text = font.render('Players: ' + str(len(player)) + '     Arrows: ' + str(len(loc_arr)) + '     Generation: '
+                           + str(gen_counter), True, (0, 0, 0), (255, 255, 255))
+        textRect = text.get_rect()
+        textRect.center = (135, 7)
+        screen.blit(text, textRect)
         # playerX += 0.3
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -192,8 +219,8 @@ def main(thr):
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_LEFT:
                     playerXchange = 0
+
         for xp in player:
-            threading.Thread(target=if_in_danger, args=(xp,), daemon=True).start()
             xp.x += xp.speed
             show_player(xp, xp.x, xp.y)
             if xp.x <= 0:
@@ -202,19 +229,28 @@ def main(thr):
                 xp.x = 715
 
         global generations
-        for arr in arrows:
+        for arr in loc_arr:
             arr.y += arrowYchange
             if arr.y < 600:
                 show_player(arr, arr.x, arr.y)
-            if if_in_range(player, arr) and arr.y <= 600:
+            if_in_range(player, arr)
+            if len(player) == 0 and arr.y <= 600:
                 if generations > 0:
+
+                    gen_counter = gen_counter + 1
                     collapse = True
                     thr.join()
-                    for ar in arrows:
+                    for t in player_threads:
+                        t.join()
+                    for ar in loc_arr:
+                        lock.acquire()
                         arrows.remove(ar)
+                        lock.release()
                     for pl in player:
                         player.remove(pl)
+                        busy.clear()
                     generations = generations - 1
+                    time.sleep(1)
                     main(StopableThread.StopableThread(target=load_arrow, args=(4,), daemon=True))
 
                 else:
@@ -222,8 +258,10 @@ def main(thr):
                     thr.join()
                     pygame.quit()
                     sys.exit()
-            elif arr.y > 600:
-                arrows.remove(arr)
+            elif arr.y > 550:
+                lock.acquire()
+                loc_arr.remove(arr)
+                lock.release()
                 #print("Arrow removed!")
 
         #if arrowY < 600:
